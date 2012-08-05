e8ca81b0974f591697851b08adecbce5f010dea1
Patrick L Archibald <patrick.archibald@gmail.com>
2012-08-05 09:52:12 -0400

    Use DataOutputStream instead of PrintWriter in Utils sendUri method.

--- Android2Desktop/src/com/pla/a2d/Utils.java
+++ Android2Desktop/src/com/pla/a2d/Utils.java
@@ -1,5 +1,6 @@
 package com.pla.a2d;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -7,20 +8,26 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
 import android.net.Uri;
+import android.util.Log;
 
 public class Utils {
+	private static final String TAG = "Utils";
+
 	public static void sendUri(String address, int port, Uri uri) throws UnknownHostException, IOException {
 		Socket clientSocket = null;
 		clientSocket = new Socket(address, port);
-		PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+		DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 		String urlString = uri.toString();
 		int urlLength = urlString.length();
 		ByteBuffer byteBuffer = ByteBuffer.allocate(urlLength + 4);
 		byteBuffer.putInt(urlLength);
 		byteBuffer.put(urlString.getBytes());
-		pw.write(new String(byteBuffer.array()));
-		pw.flush();
-		pw.close();
+		dataOutputStream.write(byteBuffer.array());
+		Log.i(TAG,
+				"Wrote ByteBuffer. URL length: " + urlLength + " url string: " + urlString + " byte buffer length: "
+						+ byteBuffer.array().length);
+		dataOutputStream.flush();
+		dataOutputStream.close();
 	}
 
 	public static int parse(String s) {
